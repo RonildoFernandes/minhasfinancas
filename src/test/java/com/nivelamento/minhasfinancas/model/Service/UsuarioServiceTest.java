7e96d73ef01c86cb4f423f4726daa576d45f0fe2
ronildo fernandes <beatrizsartorio@outlook.com>
2020-05-16 01:47:11 -0300

    falta ultima parte do service

--- src/test/java/com/nivelamento/minhasfinancas/model/Service/UsuarioServiceTest.java
+++ src/test/java/com/nivelamento/minhasfinancas/model/Service/UsuarioServiceTest.java
@@ -1,5 +1,9 @@
 package com.nivelamento.minhasfinancas.model.Service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
 import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -7,10 +11,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.nivelamento.minhasfinancas.model.Entity.Usuario;
+import com.nivelamento.minhasfinancas.model.Excepition.ErroAuteticacao;
 import com.nivelamento.minhasfinancas.model.Excepition.RegraNegocioExcepition;
 import com.nivelamento.minhasfinancas.model.Repository.UsuarioRepository;
 import com.nivelamento.minhasfinancas.model.Service.Impl.UsuarioServiceImp;
@@ -20,16 +27,61 @@ import com.nivelamento.minhasfinancas.model.Service.Impl.UsuarioServiceImp;
 @ActiveProfiles("test")
 public class UsuarioServiceTest {
 	
-	@Autowired
-	UsuariosService service;
-	@Autowired
+	@SpyBean
+	UsuarioServiceImp service;
+	@MockBean
 	UsuarioRepository repo;
 	
 	@Before
-	public void setUp() {
-		repo = Mockito.mock(UsuarioRepository.class);
+	/*public void setUp() {
+		
 		service = new UsuarioServiceImp(repo);
 	}
+	*/
+	@Test
+	public void deveAutenticarComsucesso() {
+		//cenario
+		String email = "email@email.com";
+		String senha="senha";
+		
+		Usuario usuario = Usuario.builder().email(email).senha(senha).id(1l).build();
+		Mockito.when(repo.findByEmail(email) ).thenReturn(Optional.of(usuario));
+		
+		Usuario result = service.autenticar(email, senha);
+		//verificação
+		
+		Assertions.assertNotNull(result);
+	}
+	@Test
+	public void deveLancarErroQuandoSenhaNaoBater() {
+		Assertions.assertThrows(ErroAuteticacao.class, () -> 
+		{
+			//cenário
+			String senha ="senha";
+			Usuario usuario = Usuario.builder().email("email@email.com").senha(senha).build();
+			Mockito.when(repo.findByEmail(Mockito.anyString())).thenReturn(Optional.of(usuario));
+			
+			//ação
+			service.autenticar("email@email.com", "joia");	
+			
+			
+		});
+		
+	}
+	@Test	
+	public void deveLancarerroSemUsuarioCadastrado() {
+		Assertions.assertThrows(ErroAuteticacao.class, () -> 
+		{
+			//cenário
+			Mockito.when(repo.findByEmail(Mockito.anyString())).thenReturn(Optional.empty());
+			
+			//ação
+			service.autenticar("email@email.com", "senha");
+			
+		});
+		
+		}
+	
 	
 	@Test
 	public void deveValidarEmail() {
@@ -48,8 +100,9 @@ public class UsuarioServiceTest {
 		Assertions.assertThrows(RegraNegocioExcepition.class, () -> 
 		{
 			//cenario
-			Usuario usuario = Usuario.builder().nome("usuario").email("email@email.com").build();
-			repo.save(usuario);
+			
+			Mockito.when(repo.existsByEmail(Mockito.anyString())).thenReturn(true);
+			
 		
 			//açao
 			service.validarEmail("email@email.com");
@@ -57,4 +110,23 @@ public class UsuarioServiceTest {
 		
    
  }
+	@Test
+	public void deveSalvarUsuario() {
+		//cenario
+		Mockito.doNothing().when(service).validarEmail(Mockito.anyString());
+		Usuario usuario = Usuario.builder().id(1l).nome("Ronildo").email("email@email.com").senha("senha").build();
+		
+		Mockito.when(repo.save(Mockito.any(Usuario.class))).thenReturn(usuario);
+		
+		
+		//ação
+		Usuario salvo = service.salvarUsuario(new Usuario());
+		//verificação
+		assertThat(salvo).isNotNull();
+		assertThat(salvo.getId()).isEqualTo(1l);
+		assertThat(salvo.getNome()).isEqualTo("Ronildo");
+		assertThat(salvo.getEmail()).isEqualTo("email@email.com");
+		assertThat(salvo.getSenha()).isEqualTo("senha");
+	}
+
 }
